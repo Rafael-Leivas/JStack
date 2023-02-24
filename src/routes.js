fcff91259cb072f989b05cf75c6dd02b504ff757
Rafael Leivas <rafaelblsteam@gmail.com>
2023-02-23 23:49:12 -0300

    Atualização nas rotas

--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,31 @@
 const UserController = require('./controllers/UserController')
 
 module.exports = [
+  //Está listando todos os usuários cadastrados
   {
     endpoint: '/users',
     method: 'GET',
     hendler: UserController.listUsers,
   },
+  //Filtra um usuário através do seu ID
   {
     endpoint: '/users/:id',
     method: 'GET',
     hendler: UserController.getUserById,
   },
+  //Adiciona um usuário
   {
     endpoint: '/users',
     method: 'POST',
     hendler: UserController.createUser,
   },
+  //Edita um usuário, pegando seu id e alterando as informações
   {
     endpoint: '/users/:id',
     method: 'PUT',
     hendler: UserController.updateUser,
   },
+  //Deleta o usuário, removendo o id junto com o restante das informações
   {
     endpoint: '/users/:id',
     method: 'DELETE',

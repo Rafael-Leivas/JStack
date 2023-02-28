8ad6a6186d15d32b2c745bd03957dced7139446c
Rafael Leivas <rafaelblsteam@gmail.com>
2023-02-28 11:53:15 -0300

    Iniciando o módulo de React

---
+++ React-Fundaments/build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var _user$addres;
+var user = {};
+
+//Optional chaining
+console.log(user === null || user === void 0 ? void 0 : (_user$addres = user.addres) === null || _user$addres === void 0 ? void 0 : _user$addres.street);
